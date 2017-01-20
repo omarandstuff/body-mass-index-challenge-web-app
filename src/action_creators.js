b7588bf3c78acc4b9fecda5d93dc10db81504228
David De Anda <omarandstuff@gmail.com>
2017-01-20 15:01:29 -0600

    Records acción creators

--- src/action_creators.js
+++ src/action_creators.js
@@ -60,3 +60,71 @@ export function logOut() {
     type: 'LOGOUT'
   }
 }
+
+export function loadRecords() {
+  return {
+    type: 'LOAD_RECORDS'
+  }
+}
+
+export function recordsLoaded(records) {
+  return {
+    type: 'RECORDS_LOADED',
+    records: records
+  }
+}
+
+export function recordsLoadFailed() {
+  return {
+    type: 'RECORDS_LOAD_FAILED'
+  }
+}
+
+export function createRecord(record) {
+  return {
+    type: 'CREATE_RECORD',
+    record: record
+  }
+}
+
+export function recordCreated(record) {
+  return {
+    type: 'RECORD_CREATED',
+    record: record
+  }
+}
+
+export function recordCreationFailed() {
+  return {
+    type: 'RECORD_CREATION_FAILED'
+  }
+}
+
+export function changeRecordForm(field, value) {
+  return {
+    type: 'CHANGE_RECORD_FORM',
+    field: field,
+    value: value
+  }
+}
+
+export function deleteRecord(id) {
+  return {
+    type: 'DELETE_RECORD',
+    id: id
+  }
+}
+
+export function recordDeleted(id) {
+  return {
+    type: 'RECORD_DELETED',
+    id: id
+  }
+}
+
+export function recordDeletionFailed(id) {
+  return {
+    type: 'RECORD_DELETION_FAILED',
+    id: id
+  }
+}
